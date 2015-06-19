3218ab223f7d3ce2890ad522e5b07b7ed5246853
TycheLaughs <sksouza.art@gmail.com>
2015-06-19 14:45:21 -0400

    More whitespace cleanup and comments

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+/*
+could have used the jQuery shorthand for this, but I'm not sure why I'd want
+ to when this works just fine
+*/
 document.getElementById("progressQuiz").addEventListener("click", function(){
-   //toggle quiz heights
+   /*toggle the two quizzes, while still keeping the elements in the DOM*/
    document.getElementById("firstQuiz").classList.toggle("hide");
    document.getElementById("secondQuiz").classList.toggle("hide");
 });
 document.getElementById("submitButton").addEventListener("click",function(){
+   /* toggle the result content and landing page*/
    document.getElementById("main").classList.toggle("hide");
    document.getElementById("launcher").classList.toggle("hide");
    document.getElementById("page-1").classList.toggle("hide");
